dba57fe24d91929a58342d98b3011a7d80202f7f
MVsingularity <60405343+MVsingularity@users.noreply.github.com>
2022-09-01 21:01:02 -0700

    Users can see all mock events

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
-app.get ('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+
+
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn"},
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
+
+app.get('/', (req, res) => {
+  res.render('events-index', { events: events });
 })
+
 
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
